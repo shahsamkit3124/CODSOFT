17078c3f6e6cd0337e0cf090c78b8a1032b60545
Samkit Shah <98453115+shahsamkit3124@users.noreply.github.com>
2023-09-24 02:03:21 +0530

    Update Task 1.cpp

--- Task 1.cpp
+++ Task 1.cpp
@@ -1,3 +1,7 @@
+/* Create a program that generates a random number and asks the
+user to guess it. Provide feedback on whether the guess is too
+high or too low until the user guesses the correct number.*/
+
 #include <iostream>
 #include <ctime>
 
