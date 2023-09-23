5802b5c8754176d4c4a351603907e046f0f95a7a
Samkit Shah <98453115+shahsamkit3124@users.noreply.github.com>
2023-09-24 02:04:22 +0530

    Update Task 2.cpp

--- Task 2.cpp
+++ Task 2.cpp
@@ -1,3 +1,8 @@
+/*Develop a calculator program that performs basic arithmetic
+operations such as addition, subtraction, multiplication, and
+division. Allow the user to input two numbers and choose an
+operation to perform.*/
+
 #include <iostream>
 
 using namespace std;
