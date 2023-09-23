79d21583ca72c509c1ccd824772ebb3015b42c3a
Samkit Shah <98453115+shahsamkit3124@users.noreply.github.com>
2023-09-24 02:05:13 +0530

    Update Task 3.cpp

--- Task 3.cpp
+++ Task 3.cpp
@@ -1,3 +1,8 @@
+/*Create a program that manages student grades. Allow the user
+to input student names and their corresponding grades.
+Calculate the average grade and display it along with the highest
+and lowest grades*/
+
 #include <iostream>
 #include <string>
 
